246b9b3da9cf13b0aae41f4789f08b25050d2aeb
murielledouay <guyard_murielle@orange.fr>
2020-05-07 14:53:57 +0200

    PHP Partie 2 exo 4

--- index.php
+++ index.php
@@ -1,24 +1,19 @@
 <?php
 // traduction en else if de l'opérateur ternaire
-$age = 140;
+$age = 10;
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
   <meta charset="utf-8"/>
   <link rel="stylesheet" href="assets/css/style.css"/>
-  <title>Partie 2 exo 3</title>
+  <title>Partie 2 exo 4</title>
 </head>
 <body>
-  <?php
-  if ($age >=18 && $age <120){
-      echo 'Tu es majeur';
-    } elseif ($age <18 && $age >0){
-      echo 'Tu n\'es pas majeur';
-    }
-    else {
-      echo 'Tu as fait une erreur dans la saisie de ton âge';
-    }
-  ?>
+  <?php if ($age >=18){ ?>
+    <p>Tu es majeur</p>
+  <?php } else { ?>
+  <p>Tu n'es pas majeur</p>
+  <?php } ?>
 </body>
 </html>
