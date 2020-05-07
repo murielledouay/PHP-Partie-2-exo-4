c3115e6a0a0d5801b716810530ebba24d9507d2b
murielledouay <guyard_murielle@orange.fr>
2020-05-07 12:02:11 +0200

    PHP Partie 2 exo 4

--- index.php
+++ index.php
@@ -1,28 +1,24 @@
 <?php
-//déclaration de la variable number avec sa valeur 140 et des variables enchaînant l'addition et la division
-$number = 140;
-$number = $number + 30;
-$number = $number / 2;
+// traduction en else if de l'opérateur ternaire
+$age = 140;
 ?>
-
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-
 <head>
   <meta charset="utf-8"/>
   <link rel="stylesheet" href="assets/css/style.css"/>
-  <title>Partie 1 exo 6</title>
+  <title>Partie 2 exo 3</title>
 </head>
-
 <body>
-
-  <p>Résultat :
-    <?php
-    //j'affiche avec echo le résultat du calcul ave la variable number
-    echo $number;
-    ?>
-  </p>
-
-
+  <?php
+  if ($age >=18 && $age <120){
+      echo 'Tu es majeur';
+    } elseif ($age <18 && $age >0){
+      echo 'Tu n\'es pas majeur';
+    }
+    else {
+      echo 'Tu as fait une erreur dans la saisie de ton âge';
+    }
+  ?>
 </body>
 </html>
